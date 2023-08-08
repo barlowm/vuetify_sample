b2cd700ed0bdfe91507049ac51160174f0218326
Mike Barlow <barlowm@gmail.com>
2023-08-08 13:51:21 -0400

    Removed the "HelloWorld" file and references and replaced with "Home" This is a rudimentary change just to start checking things out and providing a process outline moving forward

---
+++ src/components/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-center text-center fill-height">
+    <h1>This will be my new Home Page</h1>
+    </v-responsive>
+  </v-container>
+</template>
